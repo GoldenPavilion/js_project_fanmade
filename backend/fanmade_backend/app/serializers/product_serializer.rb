6767faddbec87a9aaca4fd45c5e066a02de00805
GoldenPavilion <rcorystine@gmail.com>
2021-09-17 18:17:30 -0400

    added category association to product serializer attrs

--- backend/fanmade_backend/app/serializers/product_serializer.rb
+++ backend/fanmade_backend/app/serializers/product_serializer.rb
@@ -1,4 +1,4 @@
 class ProductSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :company, :fandom, :price, :description, :link
+  attributes :name, :company, :fandom, :price, :description, :link, :category_id, :category
 end
