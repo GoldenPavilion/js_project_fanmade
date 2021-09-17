50f4889fa72b130d29d223189c1cef3cb26ffca7
GoldenPavilion <rcorystine@gmail.com>
2021-09-17 18:56:56 -0400

    created a create action and category_params in categories controller

--- backend/fanmade_backend/app/controllers/api/categories_controller.rb
+++ backend/fanmade_backend/app/controllers/api/categories_controller.rb
@@ -5,4 +5,18 @@ class Api::CategoriesController < ApplicationController
         render json: categories
     end
 
+    def create
+        category = Category.new(category_params)
+        if category.save
+            render json: category
+        else
+            render json: {errors: category.errors.full_messages}
+        end
+    end
+
+    private
+
+    def category_params
+        params.require(:category).permit(:name)
+    end
 end
