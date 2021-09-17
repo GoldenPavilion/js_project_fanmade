c58b66b7adc7976fbeb7d6f054eee421b46cc618
GoldenPavilion <rcorystine@gmail.com>
2021-09-17 16:38:31 -0400

    build out basic create action and strong params in ProductsController

--- backend/fanmade_backend/app/controllers/api/products_controller.rb
+++ backend/fanmade_backend/app/controllers/api/products_controller.rb
@@ -3,4 +3,14 @@ class Api::ProductsController < ApplicationController
         products = Product.all
         render json: products
     end
+
+    def create
+        product = Product.new(product_params)
+    end
+
+    private
+
+    def product_params
+        params.require(:product).permit(:name, :company, :fandom, :price, :description, :link, :category_id)
+    end
 end
