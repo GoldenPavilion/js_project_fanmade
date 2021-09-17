7b62a84fadae0be3d750fa3b26adfc018f86ffcb
GoldenPavilion <rcorystine@gmail.com>
2021-09-17 16:49:03 -0400

    added create route to product resources

--- backend/fanmade_backend/config/routes.rb
+++ backend/fanmade_backend/config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   namespace :api do
-    resources :products, only: [:index]
+    resources :products, only: [:index, :create]
   end
 end
