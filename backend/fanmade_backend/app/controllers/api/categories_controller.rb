8bb4457f4c2ef6169084ae914d39e9fcb4e3f8ca
GoldenPavilion <rcorystine@gmail.com>
2021-09-17 19:11:44 -0400

    integrated category serializer into categories controller

--- backend/fanmade_backend/app/controllers/api/categories_controller.rb
+++ backend/fanmade_backend/app/controllers/api/categories_controller.rb
@@ -2,7 +2,7 @@ class Api::CategoriesController < ApplicationController
 
     def index
         categories = Category.all
-        render json: categories
+        render json: CategorySerializer.new(categories)
     end
 
     def create
