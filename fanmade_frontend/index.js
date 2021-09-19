2e75adb0f8b9e8a3e98affce733c05c1d2502712
GoldenPavilion <rcorystine@gmail.com>
2021-09-19 15:45:35 -0400

    built out fetchProducts function, but not working in console

--- fanmade_frontend/index.js
+++ fanmade_frontend/index.js
@@ -1,1 +1,11 @@
-console.log("verified");
+document.addEventListener('DOMContentLoaded', () => {
+    fetchProducts();
+})
+
+function fetchProducts(){
+    fetch("http://localhost:3000/api/products")
+    .then(response => response.json)
+    .then(products => {
+        console.log(products);
+    })
+}
