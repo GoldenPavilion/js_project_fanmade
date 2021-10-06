f1368d1012ae3980940f8911cb0aead42d9506a2
GoldenPavilion <rcorystine@gmail.com>
2021-10-06 12:15:57 -0400

    created skeleton of product class

---
+++ fanmade_frontend/product.js
@@ -0,0 +1,11 @@
+class Product {
+    constructor(name, company, fandom, price, description, link, category_id){
+        this.name = name;
+        this.company = company;
+        this.fandom = fandom;
+        this.price = price;
+        this.description = description;
+        this.link = link;
+        this.category_id = category_id;
+    }
+}
