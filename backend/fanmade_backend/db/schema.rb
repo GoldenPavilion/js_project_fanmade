bd67bfb3ba45e42dad0c75c7f4927df17dd25f06
GoldenPavilion <rcorystine@gmail.com>
2021-10-07 20:30:19 -0400

    created fandom table and migration

--- backend/fanmade_backend/db/schema.rb
+++ backend/fanmade_backend/db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_07_222956) do
+ActiveRecord::Schema.define(version: 2021_10_08_002703) do
 
   create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "fandoms", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
