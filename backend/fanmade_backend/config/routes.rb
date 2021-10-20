5d691e41a694ec9b32ce47d805b6c6c883d33697
GoldenPavilion <rcorystine@gmail.com>
2021-10-19 20:12:06 -0400

    moved destroy from categories to products - bug still not fixed

--- backend/fanmade_backend/config/routes.rb
+++ backend/fanmade_backend/config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   namespace :api do
-    resources :products, only: [:index, :create, :show]
-    resources :categories, only: [:index, :create, :show, :destroy]
+    resources :products, only: [:index, :create, :show, :destroy]
+    resources :categories, only: [:index, :create, :show]
     resources :fandoms, only: [:index, :show]
   end
 end
